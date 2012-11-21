b44b97a0953f9afa7da3ac1311d9d748a13ca724
Collin <collin.forrester@gmail.com>
2012-11-20 19:48:39 -0600

    new BlackCardInPlayView (not used yet)

---
+++ js/views/BlackCardInPlayView.js
@@ -0,0 +1,35 @@
+define(['jquery', 'backbone', 'collections/BlackCardsCollection'], function($, Backbone, BlackCardsCollection){
+  var View = Backbone.View.extend({
+
+    el: "section#blackCardInPlay",
+
+    initialize: function() {
+      var self = this;
+      this.collection.on( 'add remove change set', function( data ) {
+        self.render();
+      });
+    },
+
+    events: {
+    },
+
+    render: function() {
+      console.log( '%BlackCardInPlayView.render()', 'color: blue;' );
+      this.template = _.template( $("#black-card-in-play-view").html(),  {  cards: this.collection } );
+      console.log( this.template);
+      this.$el.html(this.template);
+      return this;
+    },
+
+    addCard: function( cardModel ) {
+      this.collection.add( cardModel );
+    },
+
+    removeCard: function( cardModel ) {
+      this.collection.remove( cardModel );
+    }
+  });
+
+  // Returns the View class
+  return View;
+});
